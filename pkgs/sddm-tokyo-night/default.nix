43a6847b981a542bf22aad1af1194beb5534bbd3
wandabastyle <wandabastyle@gmail.com>
2026-05-30 05:20:45 +0200

    feat(config): add Tokyo Night background image and update theme config
    - Added new background image `wp6265738.png` to assets and integrated it into the SDDM theme build
    - Updated `default.nix` to explicitly copy the background image to `Backgrounds/` directory
    - Modified `theme.conf` via `substituteInPlace` to reference the new background instead of `tokyocity.png`
    - Refactored theme directory creation for clarity by using a `themeDir` variable

--- pkgs/sddm-tokyo-night/default.nix
+++ pkgs/sddm-tokyo-night/default.nix
@@ -5,12 +5,18 @@ stdenvNoCC.mkDerivation {
   version = "local";
 
   src = ../../assets/sddm/tokyo-night-sddm;
+  background = ../../assets/sddm/wp6265738.png;
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/share/sddm/themes/tokyo-night-sddm
-    cp -r . $out/share/sddm/themes/tokyo-night-sddm
+    themeDir=$out/share/sddm/themes/tokyo-night-sddm
+    mkdir -p $themeDir
+    cp -r . $themeDir
+
+    cp $background $themeDir/Backgrounds/wp6265738.png
+    substituteInPlace $themeDir/theme.conf \
+      --replace 'Background="Backgrounds/tokyocity.png"' 'Background="Backgrounds/wp6265738.png"'
 
     runHook postInstall
   '';
