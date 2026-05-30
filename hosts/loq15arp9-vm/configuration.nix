1c982838908fb0d7a6d2c8a9ff827b165788e561
wandabastyle <wandabastyle@gmail.com>
2026-05-30 02:51:47 +0200

    feat(vm): add hardware-configuration.nix with VirtIO support
    Replace laptop hardware config with VM-specific modules: virtio, 9p, no firmware

--- hosts/loq15arp9-vm/configuration.nix
+++ hosts/loq15arp9-vm/configuration.nix
@@ -11,7 +11,7 @@ let
 in
 {
   imports = [
-    ../loq15arp9/hardware-configuration.nix
+    ./hardware-configuration.nix
     ./disko.nix
   ];
 
---
+++ hosts/loq15arp9-vm/hardware-configuration.nix
@@ -0,0 +1,31 @@
+{ lib, ... }:
+
+{
+  # VM-specific hardware configuration
+  # This replaces the physical laptop hardware-config for QEMU/KVM VMs
+
+  boot.initrd.availableKernelModules = [
+    "virtio_pci"
+    "virtio_blk"
+    "virtio_scsi"
+    "virtio_net"
+    "virtio_balloon"
+    "virtio_console"
+    "virtio_rng"
+    "9p"
+    "9pnet_virtio"
+    "xhci_pci"
+    "usb_storage"
+    "sr_mod"
+  ];
+
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  # No firmware needed in VM
+  hardware.enableRedistributableFirmware = lib.mkDefault false;
+
+  # Generic x86_64 for VM
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
